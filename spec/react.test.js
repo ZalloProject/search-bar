f56ca7b18916af770a5132e86112d324e568faed
Hack Reactor Student <student@hackreactor.com>
2019-04-11 11:45:36 -0700

    updated tests for view refactor

--- spec/react.test.js
+++ spec/react.test.js
@@ -10,7 +10,7 @@ describe('Search component', () => {
   test('It should change state when price div clicked', done => {
     const wrapper = mount(<Search />);
     wrapper.find('.priceP').simulate('click');
-    expect(wrapper.state('priceCheck')).toBe(true);
+    expect(wrapper.state('view')).toBe('prices');
     done();
   });
   test('Price component renders correct string when app mounts', done => {
@@ -21,7 +21,7 @@ describe('Search component', () => {
   });
   test('Price dropdown renders when priceCheck in state is true', done => {
     const wrapper = mount(<Search />);
-    wrapper.setState({ priceCheck: true }, () => {
+    wrapper.setState({ view: 'prices' }, () => {
       const firstLowPrice = wrapper.find('.firstLowPrice').length;
       const lowPrices = wrapper.find('.lowPrice').length;
       expect(firstLowPrice + lowPrices).toBe(9);
@@ -30,7 +30,7 @@ describe('Search component', () => {
   });
   test('The low price changes on state when clicked', done => {
     const wrapper = mount(<Search />);
-    wrapper.setState({ priceCheck: true }, () => {
+    wrapper.setState({ view: 'prices' }, () => {
       const before = wrapper.state('priceLow');
       wrapper.find('#search_twoP').simulate('click');
       setTimeout(() => {
@@ -42,7 +42,7 @@ describe('Search component', () => {
   });
   test('Price dropdown renders high prices when priceCheck/highPriceCheck in state is true', done => {
     const wrapper = mount(<Search />);
-    wrapper.setState({ priceCheck: true, highPriceCheck: true }, () => {
+    wrapper.setState({ view: 'prices', highPriceCheck: true }, () => {
       const firstHighPrice = wrapper.find('.firstHighPrice').length;
       const highPrices = wrapper.find('.highPrice').length;
       expect(firstHighPrice + highPrices).toBe(9);
@@ -51,7 +51,7 @@ describe('Search component', () => {
   });
   test('High price changes in state with the high price is clicked on', done => {
     const wrapper = mount(<Search />);
-    wrapper.setState({ priceCheck: true, highPriceCheck: true }, () => {
+    wrapper.setState({ view: 'prices', highPriceCheck: true }, () => {
       const before = wrapper.state('priceHigh');
       wrapper.find('#search_firstHigh').simulate('click');
       setTimeout(() => {
@@ -63,7 +63,7 @@ describe('Search component', () => {
   });
   test('Beds dropdown renders when bedsCheck in state is true', done => {
     const wrapper = mount(<Search />);
-    wrapper.setState({ bedsCheck: true }, () => {
+    wrapper.setState({ view: 'beds' }, () => {
       const bedSpans = wrapper.find('.bedSpan').length;
       expect(bedSpans).toBe(7);
       done();
@@ -71,7 +71,7 @@ describe('Search component', () => {
   });
   test('State for bed count changes when bed span is clicked on', done => {
     const wrapper = mount(<Search />);
-    wrapper.setState({ bedsCheck: true }, () => {
+    wrapper.setState({ view: 'beds' }, () => {
       const bedsBefore = wrapper.state('beds');
       wrapper.find('#search_three3').simulate('click');
       setTimeout(() => {
@@ -89,14 +89,14 @@ describe('Search component', () => {
   });
   test('The home option drop down should render', done => {
     const wrapper = mount(<Search />);
-    wrapper.setState({ homeCheck: true }, () => {
+    wrapper.setState({ view: 'homes' }, () => {
       expect(wrapper.find('.houseSpan').length).toBe(4);
       done();
     });
   });
   test('Clicking home option houses check box should change state', done => {
     const wrapper = mount(<Search />);
-    wrapper.setState({ homeCheck: true }, () => {
+    wrapper.setState({ view: 'homes' }, () => {
       const before = wrapper.state('houses');
       wrapper.find('#search_houses').simulate('click');
       setTimeout(() => {
@@ -108,7 +108,7 @@ describe('Search component', () => {
   });
   test('Clicking home option apartment check box should change state', done => {
     const wrapper = mount(<Search />);
-    wrapper.setState({ homeCheck: true }, () => {
+    wrapper.setState({ view: 'homes' }, () => {
       const before = wrapper.state('apts');
       wrapper.find('#search_Apts').simulate('click');
       setTimeout(() => {
@@ -120,7 +120,7 @@ describe('Search component', () => {
   });
   test('Clicking home option condo check box should change state', done => {
     const wrapper = mount(<Search />);
-    wrapper.setState({ homeCheck: true }, () => {
+    wrapper.setState({ view: 'homes' }, () => {
       const before = wrapper.state('condos');
       wrapper.find('#search_condo').simulate('click');
       setTimeout(() => {
@@ -132,7 +132,7 @@ describe('Search component', () => {
   });
   test('Clicking home option townhome check box should change state', done => {
     const wrapper = mount(<Search />);
-    wrapper.setState({ homeCheck: true }, () => {
+    wrapper.setState({ view: 'homes' }, () => {
       const before = wrapper.state('townHomes');
       wrapper.find('#search_thomes').simulate('click');
       setTimeout(() => {
